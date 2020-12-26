718cac72a5e6c05c84e4c06db02a2c6aa70dceb1
rik <p3mal88@gmail.com>
2020-12-26 20:14:51 +0100

    Remove useless files

--- tavern/hooks/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(filename="example.log", encoding='utf-8', level=logging.DEBUG)
-
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
