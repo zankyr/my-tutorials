d8fece26394fc7d6c7272f419b7e89c73b4ad201
rik <p3mal88@gmail.com>
2020-12-26 20:11:30 +0100

    Add hooks module

---
+++ tavern/hooks/conftest.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(filename="logfile.log", encoding='utf-8', level=logging.INFO)
+
+def pytest_tavern_beta_after_every_response(expected, response): 
+    logging.info("===== RESPONSE ======")
+    logging.info(response)
+    logging.info(response.text)
+    logging.info(response.json)
+
+    logging.info("===== EXPECTED ======")
+    logging.info(expected)
+   
---
+++ tavern/hooks/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(filename="example.log", encoding='utf-8', level=logging.DEBUG)
+
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
