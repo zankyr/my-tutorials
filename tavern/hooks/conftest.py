5352ef5fd14ff34f9fc958d6a2fc4cb941036645
rik <p3mal88@gmail.com>
2020-12-26 20:22:25 +0100

    Add multistage hook test

--- tavern/hooks/conftest.py
+++ tavern/hooks/conftest.py
@@ -2,7 +2,8 @@ import logging
 
 logging.basicConfig(filename="logfile.log", encoding='utf-8', level=logging.INFO)
 
-def pytest_tavern_beta_after_every_response(expected, response): 
+def pytest_tavern_beta_after_every_response(expected, response):
+    logging.info("=====< START TEST FILE >=====")
     logging.info("===== RESPONSE ======")
     logging.info(response)
     logging.info(response.text)
@@ -10,4 +11,5 @@ def pytest_tavern_beta_after_every_response(expected, response):
 
     logging.info("===== EXPECTED ======")
     logging.info(expected)
+    logging.info("=====< END TEST FILE >=====")
    
